27b4642474b9acf6ad20ac557a30d0407c47d617
Abubakkar SK <44938353+abubakkarsk212@users.noreply.github.com>
2022-02-06 18:07:39 +0530

    Create kLargestElement.cpp

---
+++ kLargestElement.cpp
@@ -0,0 +1,25 @@
+#include<queue>
+#include<vector>
+vector<int> kLargest(int input[], int n, int k){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+vector<int> ans;
+priority_queue<int,vector<int>,greater<int>> pq;
+ for(int i =0; i<k; i++){
+     pq.push(input[i]);
+ } 
+    for(int i =k; i<n;i++){
+        if(pq.top()<input[i]){
+            pq.pop();
+            pq.push(input[i]);
+        }
+    }
+    while(!pq.empty()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
