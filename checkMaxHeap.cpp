8ba0683927c9abe98df69dbdf58f99e6ef212f1c
Abubakkar SK <44938353+abubakkarsk212@users.noreply.github.com>
2022-02-06 19:39:23 +0530

    Update checkMaxHeap.cpp

--- checkMaxHeap.cpp
+++ checkMaxHeap.cpp
@@ -1,3 +1,4 @@
+//O(nlogn)
 bool insert(int arr[], int j){
     int child = j;
     while(child>0){
@@ -11,11 +12,28 @@ bool insert(int arr[], int j){
 }
 bool isMaxHeap(int arr[], int n) {
     // Write your code here
-    for(int i =1; i<n;i++){
-     bool res = insert(arr,i);  
-        if(!res){
-            return false;
+//     for(int i =1; i<n;i++){
+//      bool res = insert(arr,i);  
+//         if(!res){
+//             return false;
+//         }
+//     }
+//     return true;
+      //traversing each nodes and check its left and right child
+    //It takes O(n)
+    for(int i = 0;i<n;i++){
+        int left = 2*i+1;
+        int right = 2*i+2;
+        if(left<n){
+            if(arr[left] >arr[i]){
+                return false;
+            }
         }
+     if(right<n){
+            if(arr[right] >arr[i]){
+                return false;
+            }
+        }   
     }
     return true;
 }
