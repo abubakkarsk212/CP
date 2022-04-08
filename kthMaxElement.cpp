e96371ba6b325252bb5ba597f7d28ec8df580783
Abubakkar SK <44938353+abubakkarsk212@users.noreply.github.com>
2022-04-08 09:21:48 +0530

    Update kthMaxElement.cpp

--- kthMaxElement.cpp
+++ kthMaxElement.cpp
@@ -1,4 +1,40 @@
 #include<queue>
+
+
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        //We should build the min heap inside constructor as we are callling only one time but if we //create inside add then will case TLE as it will be called for 10^4 times
+        
+        //2nd catch: follow this pattern to avoid Runtime error
+        size = k;
+       for(int i =0; i<nums.size();i++){
+           pq.push(nums[i]);
+           if(pq.size()>k){
+               pq.pop();
+           }
+       }
+        
+     
+    }
+    
+    int add(int val) {
+       pq.push(val);
+        if(pq.size()>size){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
 int kthLargest(int* arr, int n, int k) {
     // Write your code here
     //concept is maintain a min heap
